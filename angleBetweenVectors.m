a7702ed4bff188d75d22e13d2de2deb80ed90f2f
Leonard Wholey <lwholey@gmail.com>
2015-05-21 19:56:33 -0400

    Add tests

--- angleBetweenVectors.m
+++ angleBetweenVectors.m
@@ -6,3 +6,9 @@ crossP = v0(1) * v1(2) - v1(1) * v0(2);
 if crossP < 0
   theta = -theta;
 end
+
+%!assert (angleBetweenVectors ([1,1], [1,1]), 0.0, 10^-5)
+%!assert (angleBetweenVectors ([1,0], [1,1]), 0.78540, 10^-5)
+%!test 
+%! a = [1,1];
+%! assert (angleBetweenVectors (a, [1,0]), -0.78540, 10^-5)
